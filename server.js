70a86d3ae15b8978ae2d7bd6e608d641bd5c8693
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-04-18 18:43:51 +0300

    Add user routing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //Problem
+var Profile = require("./profile.js");
 
 //Solution
 //1.Create a web server
@@ -29,6 +30,34 @@ if (request.url === "/"){
 
 
 //3. Handle HTTP route GET /:username /chalklers
+function user(request, response) {
+  var username = request.url.replace("/", "");
+  if(username.length > 0){
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write("Header\n");
+
+    var studentProfile = new Profile(username);
+    studentProfile.on("end", function(profileJSON){
+      //Store the values
+      var values = {
+        avatarUrl : profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      };
+
+      response.write(values.username + " has" + values.badges + "\n");
+      response.end('Footer\n');
+    });
+
+    studentProfile.on("error", function(error){
+      response.write(error.message + "\n");
+      response.end('Footer\n');
+    });
+  }
+}
+
+var studentProfile = new Profile("chalkers");
   //if url ==="/..."
   //get json from Treehouse
   //on end show profile
