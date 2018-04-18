3b77a6dff0ce05a952dee43ec0166324a073a799
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-04-18 09:24:06 +0300

    Add get ip function

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-const https = require("https");
 
-function printMessage (userName, badgeCount, points){
-    const message = `${username} has ${badgeCount} total badges and ${points} points in JavaScript`;
-    console.log (message);
+const https = require('https');
+const username = "chalkers";
+
+//Function to print message to console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
 }
 
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+                          let body = "";
+                          // Read the data
+                          response.on('data', data => {
+                            body += data.toString();
+                          });
+
+                          response.on('end', () => {
+                            // Parse the data
+                            const profile = JSON.parse(body);
+                            // Print the data
+                            printMessage(username, profile.badges.length, profile.points.JavaScript);
+                          });
 
 
-function getProfile(username) {
+                          });
 
-const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
-    let body = "";
-    response.on('data', data => {
-    body += data.toString();
-  });
+'use strict';
 
-    response.on('end', () => {
-    const profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
   });
 });
-}
-
-// console.dir(process.argv);
-// const users = process.argv.slice(2);
-const users = ["antonmiroshkin", "chalkers"];
-console.log(users);
-
-getProfile("antonmiroshkin");
