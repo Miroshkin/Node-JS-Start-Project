5dce3c76b8755d8a8464c246348d86a6c5c949d2
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-04-19 19:17:24 +0300

    Simple site works

--- routes.js
+++ routes.js
@@ -1,18 +1,22 @@
 //Problem
 var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
 //Solution
 //1.Create a web server
 var http = require('http');
+
+var commonHeaders = {'Content-Type':'text/html'};
 
 //2. Handle HTTP route GET and POST i.e. Home
 function home (request, response) {
   //  if url ==="/" && GET
     if (request.url === "/"){
       //    show search
-      response.writeHead(200, {'Content-Type':'text/plain'});
-      response.write('Header\n');
-      response.write('Search\n');
-      response.end("Footer\n");
+      response.writeHead(200, commonHeaders);
+      renderer.view('header',{}, response);
+      renderer.view('search',{}, response);
+      renderer.view("footer",{}, response);
+      response.end();
     }
   // if url ==="/" POST
   //   redirect to /:username
@@ -23,8 +27,8 @@ function home (request, response) {
 function user(request, response) {
   var username = request.url.replace("/", "");
   if(username.length > 0){
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write("Header\n");
+    response.writeHead(200, commonHeaders);
+    renderer.view('header',{}, response);
 
     var studentProfile = new Profile(username);
     studentProfile.on("end", function(profileJSON){
@@ -35,14 +39,17 @@ function user(request, response) {
         badges: profileJSON.badges.length,
         javascriptPoints: profileJSON.points.JavaScript
       };
+      renderer.view('profile',values, response);
+      renderer.view('footer',{}, response);
+      response.end();
 
-      response.write(values.username + " has" + values.badges + "\n");
-      response.end('Footer\n');
     });
 
     studentProfile.on("error", function(error){
-      response.write(error.message + "\n");
-      response.end('Footer\n');
+      renderer.view('error',{errorMessage: error.message}, response);
+      renderer.view('search',{}, response);
+      renderer.view('footer',{}, response);
+      response.end();
     });
   }
 }
